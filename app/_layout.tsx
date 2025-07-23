3e9307963a89d14d1ad257f3063e91cb1c43cbe9
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2025-07-23 09:19:21 +0500

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from 'expo-router'
+import React from 'react'
+import { StyleSheet } from 'react-native'
+
+const Layout = () => {
+  return <Stack screenOptions={{ headerShown: false }}>
+    
+  </Stack>
+}
+
+export default Layout
+
+const styles = StyleSheet.create({})
