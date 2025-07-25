d3d48c4c8522a29aaf3c5af7ce9980a44ae57eda
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2025-07-25 05:25:52 +0500

    upload images on cloudinary successfully

--- services/userServices.ts
+++ services/userServices.ts
@@ -1,14 +1,25 @@
 import { firestore } from "@/config/firebase";
 import { ResponseType, UserDataType } from "@/types";
 import { doc, updateDoc } from "firebase/firestore";
+import { uploadFileToCloudinary } from "./imageServices";
 
 export const updateUser = async (
   uid: string,
   updatedData: UserDataType
 ): Promise<ResponseType> => {
   try {
-
     // image upload pending
+    if (updatedData.image && updatedData?.image?.uri) {
+      const uploadResponse = await uploadFileToCloudinary(
+        updatedData.image,
+        "users"
+      );
+      if (!uploadResponse.success) {
+        return { success: false, msg: uploadResponse.msg };
+      }
+      updatedData.image = uploadResponse.data;
+    }
+
     const userRef = doc(firestore, "users", uid);
     await updateDoc(userRef, updatedData);
     return { success: true, msg: "User updated successfully" };
